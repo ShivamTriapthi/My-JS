e244f6d54dc60f68ad56911bda78923f594c4b2d
Shivam Tripathi <shivatripathi301@gamil.com>
2026-05-31 15:22:00 +0530

    Done Successfully

---
+++ 1.js
@@ -0,0 +1,38 @@
+// let obj = {
+//   name: "JS",
+//   greet() {
+//     return `Hello ${this.name}`;
+//   }
+// };
+
+// // console.log(obj.greet)
+// let fn = obj.greet;
+// console.log(fn());
+// console.log(fn);
+
+// console.log(typeof NaN);
+// console.log(isNaN("hello"));
+// console.log(Number.isNaN("hello"));
+
+// const obj = {
+//   a: 1,
+//   b: function () {
+//     return this.a;
+//   },
+//   c: () => this.a
+// };
+
+// console.log(obj.b());
+// console.log(obj.c());
+
+const obj = {
+  a: 10,
+  getA: function () {
+    return this.a;
+  }
+};
+let x = obj.getA
+// const fn = obj.getA.bind({ a: 20 });
+console.log(obj.getA());
+// console.log(x);
+console.log(x());
---
+++ readme.md
@@ -0,0 +1,15 @@
+What is Devops 
+
+Version control system => use to manage diffrent version => ec git
+
+what is version control proper defination
+
+how to add user and email in git bash
+
+what is repo in github ?
+how to get branch name i n git bash
+how to change branch name
+
+$ git remote get-url origin => to see repo
+
+git push origin main
